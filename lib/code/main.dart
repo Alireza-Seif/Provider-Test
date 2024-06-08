4b8326d637205e077ff5f49ca33cc6788049f2c0
alirezaseif20 <alirezaariye@gmail.com>
2024-06-08 19:02:26 +0330

    fix: fix a bug for run app

--- lib/code/main.dart
+++ lib/code/main.dart
@@ -114,7 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => provider.counter = ++provider.counter,
+        onPressed: () => provider.counter = ++provider.counter, 
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider_test/code/main.dart';
 
-import 'package:provider_test/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
